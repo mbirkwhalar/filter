ea3793918a84a91e09599739b39c148c4fce4e26
Margus Birk <margus.birk@whalar.com>
2023-12-05 12:06:26 +0200

    chore: lefilter

--- filter.go
+++ filter.go
@@ -1,6 +1,9 @@
 package filter
 
-import "golang.org/x/exp/constraints"
+import (
+	"fmt"
+	"golang.org/x/exp/constraints"
+)
 
 type AllowedTypes interface {
 	constraints.Ordered
@@ -9,6 +12,7 @@ type AllowedTypes interface {
 func Filter[T AllowedTypes](slice1, slice2 []T) []T {
 	combinedSlice := append(slice1, slice2...)
 	temp := make(map[T]bool)
+	fmt.Println("Filter putting in work")
 	var filtered []T
 	for _, v := range combinedSlice {
 		_, ok := temp[v]
